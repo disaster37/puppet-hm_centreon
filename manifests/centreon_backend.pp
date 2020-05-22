d614c6e4debfe11bc5e4fb0f98040843adfdd21f
langoureaux-s <linuxworkgroup@hotmail.com>
2020-05-22 09:22:04 +0000

    Add parameter to collect all resourcesÃ

--- manifests/centreon_backend.pp
+++ manifests/centreon_backend.pp
@@ -16,6 +16,7 @@ define hm_centreon::centreon_backend (
   Boolean           $collect_service_template = true,
   Boolean           $collect_service_group = true,
   Boolean           $collect_command = true,
+  Boolean           $collect_all_config = false,
 ) {
 
   include hm_centreon
@@ -27,40 +28,76 @@ define hm_centreon::centreon_backend (
   }
 
   if $collect_host {
-    if $poller == undef {
-      Centreon_host <<| config == $name |>> {}
-    } else {
-      Centreon_host <<| config == $name |>> {
-        poller => $poller
+    if $collect_all_config {
+      if $poller == undef {
+        Centreon_host <<| |>>
+      } else {
+        Centreon_host <<| |>> {
+          poller => $poller
+        }
       }
+      Centreon_host_in_host_template <<| |>>
+      Centreon_host_in_host_group <<| |>>
+      Centreon_host_with_macro <<| |>>
+    } else {
+        if $poller == undef {
+          Centreon_host <<| config == $name |>>
+        } else {
+          Centreon_host <<| config == $name |>> {
+            poller => $poller
+          }
+        }
+      Centreon_host_in_host_template <<| config == $name |>>
+      Centreon_host_in_host_group <<| config == $name |>>
+      Centreon_host_with_macro <<| config == $name |>>
     }
-
-    Centreon_host_in_host_template <<| config == $name |>>
-    Centreon_host_in_host_group <<| config == $name |>>
-    Centreon_host_with_macro <<| config == $name |>>
   }
 
   if $collect_host_group {
-    Centreon_host_group <<| config == $name |>>
+    if $collect_all_config {
+      Centreon_host_group <<| |>>
+    } else {
+      Centreon_host_group <<| config == $name |>>
+    }
   }
 
   if $collect_host_template {
-    Centreon_host_template <<| config == $name |>>
+    if $collect_all_config {
+      Centreon_host_template <<| |>>
+    } else {
+      Centreon_host_template <<| config == $name |>>
+    }
   }
 
   if $collect_service {
-    Centreon_service <<| config == $name |>>
+    if $collect_all_config {
+      Centreon_service <<| |>>
+    } else {
+      Centreon_service <<| config == $name |>>
+    }
   }
 
   if $collect_service_group {
-    Centreon_service_group <<| config == $name |>>
+    if $collect_all_config {
+      Centreon_service_group <<| |>>
+    } else {
+      Centreon_service_group <<| config == $name |>>
+    }
   }
 
   if $collect_service_template {
-    Centreon_service_template <<| config == $name |>>
+    if $collect_all_config {
+      Centreon_service_template <<| |>>
+    } else {
+      Centreon_service_template <<| config == $name |>>
+    }
   }
 
   if $collect_command {
-    Centreon_command <<| config == $name |>>
+    if $collect_all_config {
+      Centreon_command <<| |>>
+    } else {
+      Centreon_command <<| config == $name |>>
+    }
   }
 }
